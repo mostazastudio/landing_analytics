1d40445db2b2dcc649ba33f91da1f4bd05476b40
Lobsang Mateo Peralta Sanchez <lobsangmateo@Lobsangs-MacBook-Pro.local>
2017-08-28 11:53:26 -0500

    Second commit

--- mostaza_landing/settings.py
+++ mostaza_landing/settings.py
@@ -78,9 +78,12 @@ WSGI_APPLICATION = 'mostaza_landing.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+  }
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
