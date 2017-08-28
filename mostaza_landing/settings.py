a9f0e2587fdaa5a64da1eff1c7ce725b8086341e
Lobsang Mateo Peralta Sanchez <lobsangmateo@Lobsangs-MacBook-Pro.local>
2017-08-28 13:21:56 -0500

    Second commit

--- mostaza_landing/settings.py
+++ mostaza_landing/settings.py
@@ -79,10 +79,8 @@ WSGI_APPLICATION = 'mostaza_landing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-  DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
   }
 
 
